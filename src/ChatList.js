3ad87d81ffc1dad2ea4f2273ca2b62d45485c11e
Elena <elenashevtsova380@gmail.com>
2021-07-30 20:25:33 +0300

    les6

--- src/ChatList.js
+++ src/ChatList.js
@@ -1,13 +1,16 @@
 import './Message.sass'
 import React from 'react';
 import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { useParams, useHistory } from 'react-router'
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { deleteChat } from './store/actions/chats'
 import NewChatForm from './NewChatForm'
-// import Divider from '@material-ui/core/Divider';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,11 +19,20 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.background.paper,
   },
 }));
-
 function ChatList () {
-    const chatList = useSelector(state => state.chats)
+  const { chats } = useSelector(state => state.chats);
+  const dispatch = useDispatch();
+  const params = useParams();
+  const history = useHistory();
+  const classes = useStyles();
 
-    const classes = useStyles();
+  const handleDelete = React.useCallback((event) => {
+    console.log(event.currentTarget.id, params);
+    if (event.currentTarget.id === params.chatId) {
+      history.push('/chats')
+    }
+    dispatch(deleteChat(event.currentTarget.id))
+  }, [params, dispatch, history])
 
     return (
         <div className={classes.root}>
@@ -28,12 +40,15 @@ function ChatList () {
             <ListItem button key='newChat'>
               <NewChatForm />
             </ListItem> 
-            {chatList.map((chat) => (
+            {chats?.map((chat) => (
+              <ListItem button key={chat.id}>
                 <Link key={chat.id} to={`/chats/${chat.id}`}>
-                  <ListItem button key={chat.id}>
-                      <ListItemText primary={chat.name} />  
-                  </ListItem> 
+                    <ListItemText primary={chat.name} />  
                 </Link>
+                <Button key={`btn${chat.id}`} id={chat.id} onClick={handleDelete}>
+                  <DeleteIcon />
+                </Button>
+              </ListItem> 
             ))}
           </List>
         </div>
